197ab318ee59ef6bf7cebc7f3f8510550f591843
caleb <calebcauthon@gmail.com>
2022-01-20 23:05:47 -0600

    Add octokit

--- server_side_functions/hello.js
+++ server_side_functions/hello.js
@@ -1,7 +1,13 @@
+const { Octokit } = require("@octokit/core");
+const octokit = new Octokit({ auth: process.env.GITHUB_PERSONAL_TOKEN });
+
 exports.handler = async function(event, context) {
-   return {
+  var gist_id = 'c9da1d77dd4e145d405f5c4ffebf4143'
+  var result = await octokit.request('GET /gists/{gist_id}', {
+    gist_id: gist_id
+  });
+  return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
   };
-
 }
