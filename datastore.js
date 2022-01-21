692cf3256c3f7c7663d0e26917e4d0580dd11937
caleb <calebcauthon@gmail.com>
2022-01-21 00:44:25 -0600

    Remove old code

--- datastore.js
+++ datastore.js
@@ -2,12 +2,6 @@ function getPlayers() {
   return fetch('/.netlify/functions/getPlayers')
   .then(response => {
     return response.json();
-  }).then(({ players }) => {
-    savePlayers().then(() => {
-      console.log('saved!');
-    });
-
-    return players;
   });
 }
 
