8130ec396421fe39b3da2f765416e97ac357bcdf
caleb <calebcauthon@gmail.com>
2022-01-21 00:14:27 -0600

    Start saving players

--- datastore.js
+++ datastore.js
@@ -1,8 +1,20 @@
 function getPlayers() {
-  return fetch('/.netlify/functions/hello')
+  return fetch('/.netlify/functions/getPlayers')
   .then(response => {
     return response.json();
   }).then(({ players }) => {
     return players;
+
+    savePlayers().then(() => {
+      console.log('saved!');
+    })
   });
+}
+
+
+function savePlayers() {
+  return fetch('/.netlify/functions/savePlayers', {
+    method: 'POST',
+    data: JSON.stringify(players.concat(['thing1']))
+  })
 }
---
+++ server_side_functions/savePlayers.js
@@ -0,0 +1,18 @@
+const { Octokit } = require("@octokit/core");
+const octokit = new Octokit({ auth: process.env.GITHUB_PERSONAL_TOKEN });
+
+exports.handler = async function(event, context) {
+  var gist_id = 'c9da1d77dd4e145d405f5c4ffebf4143'
+  var result = await octokit.request('PATCH /gists/{gist_id}', {
+    gist_id: gist_id,
+    files: {
+      filename: "players.json",
+      content: event.body
+    }
+  });
+
+  return {
+    statusCode: 200,
+    body: playersJson,
+  };
+}
