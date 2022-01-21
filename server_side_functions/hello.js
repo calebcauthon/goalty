7fb982b6e7966325c6e065d7b13b62e8f5a1309e
caleb <calebcauthon@gmail.com>
2022-01-20 23:07:42 -0600

    Octokit test

--- server_side_functions/hello.js
+++ server_side_functions/hello.js
@@ -8,6 +8,6 @@ exports.handler = async function(event, context) {
   });
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify(result),
   };
 }
